4ba67e0fc0fcb50a927f97c58632af771f113a83
MoeLeak <i@leak.moe>
2025-02-28 11:34:00 +0800

    Blink

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,20 @@
 #include "Arduino.h"
 
+const int ledPin = 13;
+
 void setup() {
   Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   Serial.println("Hello world!");
+  delay(1000);
+
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+
+  digitalWrite(ledPin, LOW);
   delay(1000);
 }
